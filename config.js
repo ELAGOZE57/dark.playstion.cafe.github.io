689198c838997c58910806e36f2ee34c3f8e55a8
ELAGOZE57 <elagoze57@gmail.com>
2025-10-11 12:02:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbyyT_BavvpFvkJU_4MkSmVJ1VjL-WFq-Zi4lV5aenKCYH4kW-aeyBdjMYr9jg0nRLUh/exec';
+https://script.google.com/macros/s/AKfycbwXAKvISfc_9bmCNftph7bdXyEa-bsgJN_PqSMmDdRfWbKA-i9dRc5dCvP5HHsQx-cY/exec
